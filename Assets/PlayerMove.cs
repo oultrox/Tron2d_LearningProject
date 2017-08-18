e1af8916dcac50742a98cfd2896b116d34e3242e
oultrox <jesus-perez95@hotmail.com>
2017-08-18 18:17:48 -0300

    [add] trail effect

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -6,11 +6,14 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour {
 
     [SerializeField] private float speed = 16;
+    [SerializeField] private GameObject wallPrefab;
+    private Collider2D wall;
+    private Vector2 lastWallEnd;
     private Vector3 direction;
     private Rigidbody2D rgBody;
     private float v;
     private float h;
-    
+    private GameObject g;
     
 	// Use this for initialization
 	void Start () {
@@ -30,14 +33,40 @@ public class PlayerMove : MonoBehaviour {
         {
             h = 0;
         }
-
+        SpawnWall();
         Move(h, v);
-	}
+        FitColliderBetween(wall, lastWallEnd, transform.position);
+    }
 
     private void Move(float h, float v)
     {
         direction.x = h;
         direction.y = v;
         rgBody.velocity = direction * speed;
+    }
+
+    private void SpawnWall()
+    {
+        
+        if ((direction.x != h || direction.y != v) && (h!=0 || v!=0))
+        {
+            lastWallEnd = transform.position;
+            g = Instantiate(wallPrefab, transform.position, Quaternion.identity);
+            wall = g.GetComponent<Collider2D>();
+        }
+        
+    }
+
+    void FitColliderBetween(Collider2D co, Vector2 a, Vector2 b)
+    {
+        co.transform.position = a + (b - a) * 0.5f;
+        float dist = Vector2.Distance(a, b);
+        if (a.x != b.x)
+        {
+            co.transform.localScale = new Vector2(dist + 1 , 1);
+        }else
+        {
+            co.transform.localScale = new Vector2(1 , dist + 1);
+        }
     }
 }
