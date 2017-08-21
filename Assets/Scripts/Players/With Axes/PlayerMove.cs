33a3c3c4777318d8617c6df6bec4728afc9d1f5e
oultrox <jesus-perez95@hotmail.com>
2017-08-21 15:57:44 -0300

    [add] second player / collision code

---
+++ Assets/Scripts/Players/With Axes/PlayerMove.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMove : MonoBehaviour {
+
+    [SerializeField] private string horizontalAxisName = "Horizontal"; //Default values.
+    [SerializeField] private string verticalAxisName = "Vertical";     //Default values.
+    [SerializeField] private float speed = 16;
+    [SerializeField] private GameObject wallPrefab;
+
+    private Collider2D wall;
+    private Vector2 lastWallEnd;
+    private Vector3 direction;
+    private Rigidbody2D rgBody;
+    private GameObject g;
+    private GameObject dynamicObject;
+    private float v;
+    private float h;
+    private float dist;
+    private bool isMoving;
+    
+    //----- API methods----
+	// Use this for initialization
+	void Start () {
+        rgBody = this.GetComponent<Rigidbody2D>();
+        isMoving = false;
+        dynamicObject = GameObject.FindGameObjectWithTag("DynamicObject");
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        h = Input.GetAxisRaw(horizontalAxisName);
+        v = Input.GetAxisRaw(verticalAxisName);
+        
+        //Validates 1 direction-only
+        if (h!=0)
+            v = 0;
+        else
+            h = 0;
+        
+        //If it's not moving don't spawn walls
+        if (h != 0 || v != 0)
+        {
+            SpawnWall();
+            Move(h, v);
+            
+        }
+        //Fit the spawned and connect the colliders to be trail-like.
+        FitColliderBetween(wall, lastWallEnd, transform.position);
+    }
+
+    //-----Custom methods-------
+    //Simple movement and once it's used, that means the player is moving.
+    private void Move(float h, float v)
+    {
+        //it started to move.
+        isMoving = true;
+        direction.x = h;
+        direction.y = v;
+        rgBody.velocity = direction * speed;
+    }
+
+    //Spawns the wall, since only it's working with the player's movement, it's in this class.
+    public void SpawnWall()
+    {
+        if ((direction.x != h || direction.y != v)) //Only when directions is changed
+        {
+            lastWallEnd = transform.position;
+            g = Instantiate(wallPrefab, transform.position, Quaternion.identity);
+            wall = g.GetComponent<Collider2D>();
+            g.transform.SetParent(dynamicObject.transform);
+        }
+
+    }
+    
+    //Converts the colliders into Tron-traile like, fitting them.
+    public void FitColliderBetween(Collider2D co, Vector2 a, Vector2 b)
+    {
+        if (isMoving)
+        {
+            co.transform.position = a + (b - a) * 0.5f;
+            dist = Vector2.Distance(a, b);
+
+            if (a.x != b.x)
+            {
+                wall.transform.localScale = new Vector2(dist + 1, 1);
+            }
+            else
+            {
+                wall.transform.localScale = new Vector2(1, dist + 1);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Players/With InputKeys/PlayerMovement.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    
+    // Movement keys (customizable in Inspector)
+    [SerializeField] private KeyCode upKey;
+    [SerializeField] private KeyCode downKey;
+    [SerializeField] private KeyCode rightKey;
+    [SerializeField] private KeyCode leftKey;
+    [SerializeField] private float speed = 16;
+    [SerializeField] private GameObject wallPrefab;
+    private Collider2D wall;
+    private Vector2 lastWallEnd;
+    private Vector3 direction;
+    private Rigidbody2D rgBody;
+    private GameObject g;
+    private GameObject dynamicObject;
+    private float dist;
+    private bool isMoving;
+    
+    //----- API methods------
+    // Use this for initialization
+    void Start()
+    {
+        rgBody = this.GetComponent<Rigidbody2D>();
+        dynamicObject = GameObject.FindGameObjectWithTag("DynamicObject");
+        isMoving = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for key presses
+        if (Input.GetKeyDown(upKey))
+        {
+            rgBody.velocity = Vector2.up * speed;
+            SpawnWall();
+        }
+        else if (Input.GetKeyDown(downKey))
+        {
+            rgBody.velocity = -Vector2.up * speed;
+            SpawnWall();
+        }
+        else if (Input.GetKeyDown(rightKey))
+        {
+            rgBody.velocity = Vector2.right * speed;
+            SpawnWall();
+        }
+        else if (Input.GetKeyDown(leftKey))
+        {
+            rgBody.velocity = -Vector2.right * speed;
+            SpawnWall();
+        }
+        FitColliderBetween(wall, lastWallEnd, transform.position);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col !=wall)
+        {
+            Debug.Log("Player lost: " + name);
+            Destroy(gameObject);
+        }
+    }
+    //-----Custom methods-------
+    //Spawns the wall, since only it's working with the player's movement, it's in this class.
+    private void SpawnWall()
+    {
+        isMoving = true;
+        lastWallEnd = transform.position;
+        g = Instantiate(wallPrefab, transform.position, Quaternion.identity);
+        wall = g.GetComponent<Collider2D>();
+        g.transform.SetParent(dynamicObject.transform);
+    }
+
+    //Converts the colliders into Tron-traile like, fitting them.
+    void FitColliderBetween(Collider2D co, Vector2 a, Vector2 b)
+    {
+        if (isMoving)
+        {
+            co.transform.position = a + (b - a) * 0.5f;
+            dist = Vector2.Distance(a, b);
+
+            if (a.x != b.x)
+            {
+                co.transform.localScale = new Vector2(dist + 1, 1);
+            }
+            else
+            {
+                co.transform.localScale = new Vector2(1, dist + 1);
+            }
+        }
+        
+    }
+}
